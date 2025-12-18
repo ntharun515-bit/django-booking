a911e5c0817e3fefcfcf497243b9548d02d40c66
ntharun515-bit <ntharun515@gmail.com>
2025-12-18 08:05:32 +0530

    Fix Django module path for Render deployment

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,6 +1,6 @@
 import os
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
--- core/core/settings.py
+++ core/core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core.bookings.apps.BookingsConfig',  # ✅ ONLY THIS
+    
     'bookings',
 ]
 
@@ -57,7 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.core.urls'
+ROOT_URLCONF = 'core.urls'
+
 
 
 import os
